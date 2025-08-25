99c004e2723e0ac0fc9f6ea8785e44dca6733bdf
Judd Mehr <juddmehr@gmail.com>
2025-08-25 07:28:51 -0600

    Update plot_sizing.jl

--- plotting/plot_sizing.jl
+++ plotting/plot_sizing.jl
@@ -2,36 +2,26 @@ function latexsize(x, y)
     return (x * 96, y * 96)
 end
 
-"""
-sets figure width to be 90% of the full text width divided by the number of sub figures (horizontally) and sets the height based on the input aspect ratio.
-"""
-function subfig_size(; n_cols=2, aspect_ratio=4 / 3, textwidth=7)
-    width = textwidth / n_cols * 0.9
+function subfig_size(n_cols=2; aspect_ratio=4 / 3, text_width=7.0)
+    width = text_width / n_cols
     return latexsize(width, width / aspect_ratio)
 end
 
-"""
-sets the figure width to be about the text width for a 2 column article
-"""
+
 function two_column_size(; aspect_ratio=4 / 3)
     return latexsize(3.5, 3.5 / aspect_ratio)
 end
 
-"""
-my default for single figures in a single column article
-"""
 function one_column_default(; aspect_ratio=4 / 3)
     return latexsize(5, 5 / aspect_ratio)
 end
 
-"""
-about full text width for a single column article
-"""
 function one_column_full_width(; aspect_ratio=4 / 3)
     return latexsize(7, 7 / aspect_ratio)
 end
 
-#TODO; for presentation animations, need to adjust more than figure size, probably need to write a defaults function that changes font sizes and line thickness as well
+#TODO; for presentations, need to adjust font size
+#TODO: really, for presentations need to have a whole different default function probably.
 function presentation_full_width(; aspect_ratio=16 / 9)
     return latexsize(16, 16 / aspect_ratio)
 end
@@ -39,3 +29,5 @@ end
 function presentation_half_width(; aspect_ratio=8 / 9)
     return latexsize(8, 8 / aspect_ratio)
 end
+
+aspect_ratio_1_tight = Dict(:subplot => Dict("axis equal image" => nothing))
