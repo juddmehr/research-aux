22735fd76d86654631f1b5557e368830bacdaaf8
Judd Mehr <juddmehr@gmail.com>
2025-08-25 07:27:17 -0600

    Update plots_default.jl

--- plotting/plots_default.jl
+++ plotting/plots_default.jl
@@ -1,12 +1,13 @@
 using Plots
 pgfplotsx() # use PGFPlotsX backend for LaTeX plots
-using LaTeXStrings
-using Measures
+using LaTeXStrings # for math in axis labels and annotations
+using Measures # convenient for setting margins in mm
 
 default(;
     # - Fonts - #
     fontfamily="cmunrm", # computer modern roman
     annotationfontfamily="cmunrm",  # computer modern roman
+    guidefontfamily="cmunrm",
     # - Turn off Backgrounds - #
     background_color=nothing, # transparent outside of axes
     background_color_inside=nothing, # transparent inside of axes
@@ -23,7 +24,9 @@ default(;
         RGB(190 / 255, 147 / 255, 61 / 255), # yellow #be933d
         RGB(128 / 255, 128 / 255, 128 / 255), # middle gray #808080
     ],
-    # - Misc - #
+    # - Labels - #
+    guidefontsize=11, # axis label font size
+    tickfontsize=8, # tick font size
     yguidefontrotation=-90, # rotates y-axis label
 )
 
