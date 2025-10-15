3857e123a6c00cd231a34912333bdef88a4e4384
Judd Mehr <juddmehr@gmail.com>
2025-10-14 19:00:35 -0600

    Update plots_default.jl
    Add convenient functions

--- plotting/plots_default.jl
+++ plotting/plots_default.jl
@@ -30,3 +30,66 @@ default(;
     yguidefontrotation=-90, # rotates y-axis label
 )
 
+# color palatte for easy access
+primary = RGB(0, 92 / 255, 171 / 255) #blue #005cab
+secondary = RGB(190 / 255, 76 / 255, 77 / 255) #red #be4b4d
+tertiary = RGB(105 / 255, 174 / 255, 95 / 255) #green #6aae5f
+quaternary = RGB(167 / 255, 84 / 255, 164 / 255) #purple #a754a4
+quinary = RGB(190 / 255, 147 / 255, 61 / 255) #yellow #be933d
+plotsgray = RGB(128 / 255, 128 / 255, 128 / 255) #gray #808080
+
+# Useful extra_kwarg to actually make an aspect_ratio=:equal PGFPlotsX plot
+aspect_ratio_1_tight = Dict(:subplot => Dict("axis equal image" => nothing))
+
+# cutom color map functions that let you place the center color at zero, etc.
+function diverging_cgrad(
+    low_cutoff,
+    high_cutoff;
+    low_color=primary,
+    high_color=secondary,
+    low_flag_color=quaternary,
+    high_flag_color=tertiary,
+    zero_alpha=0.0,
+    df = sqrt(eps())
+)
+    return cgrad(
+        # The colors for the gradient
+        [
+            low_flag_color,
+            low_color,
+            RGBA(1.0, 1.0, 1.0, zero_alpha),
+            high_color,
+            high_flag_color,
+        ],
+        # The values where each color is placed (linearly interpolated from 0 to 1 as required)
+        (
+            [low_cutoff - df, low_cutoff, 0.0, high_cutoff, high_cutoff + df] .-
+            (low_cutoff - df)
+        ) / (high_cutoff + df - (low_cutoff - df)),
+    )
+end
+
+function sequential_cgrad(
+    low_cutoff,
+    high_cutoff;
+    high_color=primary,
+    low_flag_color=quaternary,
+    high_flag_color=secondary,
+    low_alpha=0.0,
+    df=sqrt(eps())
+)
+    return cgrad(
+        # The colors for the gradient
+        [
+            low_flag_color,
+            RGBA(1.0, 1.0, 1.0, low_alpha),
+            high_color,
+            high_flag_color,
+        ],
+        # The values where each color is placed (linearly interpolated from 0 to 1 as required)
+        (
+            [low_cutoff - df, low_cutoff, high_cutoff, high_cutoff + df] .-
+            (low_cutoff - df)
+        ) / (high_cutoff + df - (low_cutoff - df)),
+    )
+end
